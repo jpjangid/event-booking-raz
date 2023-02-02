23dde2ad7ba5d6c6947f3e16d19d6d574b375328
kinal kukda <mohsin@webanix.in>
2023-02-02 23:34:48 +0530

    commit

--- src/app/components/common/event-details/event-details.component.ts
+++ src/app/components/common/event-details/event-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
@@ -7,48 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router , private activatedRoute : ActivatedRoute) { }
 
+  data : any;
   ngOnInit(): void {
+    let data1 = this.activatedRoute.snapshot.params;
+    if(data1.id){
+     this.data =  this.packages.filter(res=> res.id == data1.id)
+    }
+
+    console.log(this.data);
   }
 
   images: any = {
     "data":[
         {
-            "previewImageSrc": "assets/img/category/category1.jpg",
-            "thumbnailImageSrc": "assets/img/category/category1.jpg",
+            "previewImageSrc": "assets/img/dubai.jfif",
+            "thumbnailImageSrc": "assets/img/dubai.jfif",
             "name": '3N Dubai Best Deal',
             "price": 'INR 17,290'
         },
         {
-            "previewImageSrc": "assets/img/blog/blog2.jpg",
-            "thumbnailImageSrc": "assets/img/blog/blog2.jpg",
+            "previewImageSrc": "assets/img/dubai2.jpg",
+            "thumbnailImageSrc": "assets/img/dubai2.jpg",
              "name": '3N Dubai Best Deal',
              "price": 'INR 17,290'
         },
         {
-            "previewImageSrc": "assets/img/blog/blog3.jpg",
-            "thumbnailImageSrc": "assets/img/blog/blog3.jpg",
-            "name": '3N Dubai Best Deal',
-            "price": 'INR 17,290'
-        },
-        {
-            "previewImageSrc": "assets/img/category/category1.jpg",
-            "thumbnailImageSrc": "assets/img/category/category4.jpg",
+            "previewImageSrc": "assets/img/dubai3.jpg",
+            "thumbnailImageSrc": "assets/img/dubai3.jpg",
              "name": '3N Dubai Best Deal',
              "price": 'INR 17,290'
-        },
-        {
-            "previewImageSrc": "assets/img/category/category1.jpg",
-            "thumbnailImageSrc": "assets/img/category/category5.jpg",
-             "name": '3N Dubai Best Deal',
-             "price": 'INR 17,290'
-        },
-        {
-            "previewImageSrc": "assets/img/category/category1.jpg",
-            "thumbnailImageSrc": "assets/img/category/category1.jpg",
-            "name": '3N Dubai Best Deal',
-            "price": 'INR 17,290'
         }
     ]
   }
@@ -56,51 +46,78 @@ export class EventDetailsComponent implements OnInit {
   packages: any = [
     {
       id: 1,
-      image: 'assets/img/dubai/image1.jpg',
       name: '3N Dubai Best Deal',
       price: 'INR 20,290',
       images : this.images
     },
     {
       id: 2,
-      image: 'assets/img/dubai/image1.jpg',
-      name: '3N London Best Deal',
+      name: '4N Dubai Luxury',
+      price: 'INR 20,290',
+      images : this.images
+    },
+    {
+      id: 3,
+      name: '5N Dubai Masti',
       price: 'INR 22,290',
       images : this.images
     },
     {
-      id: 1,
-      image: 'assets/img/dubai/image1.jpg',
-      name: '3N Paris Best Deal',
+      id: 4,
+      name: '3N Singapore Fun',
       price: 'INR 25,290',
       images : this.images
     },
     {
-      id: 1, 
-      image: 'assets/img/dubai/image1.jpg',
-      name: '3N Dubai Best Deal',
+      id: 5, 
+      name: '4N Singapore Masti',
+      price: 'INR 50,290',
+      images : this.images
+    },
+    {
+      id: 6, 
+      name: '3N Bali Best Deal',
+      price: 'INR 50,290',
+      images : this.images
+    },
+    {
+      id: 7, 
+      name: '5N Singapore Chill',
+      price: 'INR 50,290',
+      images : this.images
+    },
+    {
+      id: 8, 
+      name: '5N Best of Bali',
+      price: 'INR 50,290',
+      images : this.images
+    },
+    {
+      id: 9, 
+      name: '4N Malaysia Blast',
       price: 'INR 50,290',
       images : this.images
     }
   ]
 
-    responsiveOptions:any[] = [
-        {
-            breakpoint: '1024px',
-            numVisible: 5
-        },
-        {
-            breakpoint: '960px',
-            numVisible: 4
-        },
-        {
-            breakpoint: '768px',
-            numVisible: 3
-        },
-        {
-            breakpoint: '560px',
-            numVisible: 1
-        }
-    ];
+
+  responsiveOptions:any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '960px',
+          numVisible: 4
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
 
 }
