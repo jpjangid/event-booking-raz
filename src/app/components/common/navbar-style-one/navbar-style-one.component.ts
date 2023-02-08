9032396e0d6cebaaa77dc76a332bf83b9d875ab0
jpjangid <102713524+jpjangid@users.noreply.github.com>
2023-02-08 19:52:39 +0530

    change

--- src/app/components/common/navbar-style-one/navbar-style-one.component.ts
+++ src/app/components/common/navbar-style-one/navbar-style-one.component.ts
@@ -61,8 +61,8 @@ export class NavbarStyleOneComponent implements OnInit {
   }
 
   bookEvent() {
-    window.location.href = 'https://api.adorntourism.com/api/Dropdowns/Registration?Mode=upline'
-    // this.router.navigateByUrl('/customerBooking');
+    // window.location.href = 'https://api.adorntourism.com/api/Dropdowns/Registration?Mode=upline'
+    this.router.navigateByUrl('/customerBooking');
   }
 
   logOut(){
